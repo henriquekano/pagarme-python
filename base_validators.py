8df50a98fa1d55de0455d82fd9315b6e46b0afac
Henrique Kano <henriquekano@hotmail.com>
2017-02-09 20:59:13 -0200

    Added new base validators

--- base_validators.py
+++ base_validators.py
@@ -21,3 +21,35 @@ def check_in_enum(parameter, parameter_enum, parameters_list):
             'type': 'format',
             'message': (parameter + ' should be one of: [' + ','.join(parameter_enum) + ']')
         }
+
+def check_max(parameter, max_value, parameters_list):
+    if(parameters_list[parameter] > max_value):
+        return {
+            'parameter': parameter,
+            'type': 'format',
+            'message': (parameter + ' should be at most ' + str(max_value))
+        }
+
+def check_min(parameter, min_value, parameters_list):
+    if(parameters_list[parameter] < min_value):
+        return {
+            'parameter': parameter,
+            'type': 'format',
+            'message': (parameter + ' should be at minimum ' + str(max_value))
+        }
+
+def check_max_length(parameter, max_length, parameters_list):
+    if(len(parameters_list[parameter]) > max_length):
+        return {
+            'parameter': parameter,
+            'type': 'format',
+            'message': (parameter + ' should be at most ' + str(max_length) + ' characters long')
+        }
+
+def check_min_length(parameter, min_length, parameters_list):
+    if(len(parameters_list[parameter]) < min_length):
+        return {
+            'parameter': parameter,
+            'type': 'format',
+            'message': (parameter + ' should be at minimum ' + str(min_length) + ' characters long')
+        }
--- validators.py
+++ validators.py
@@ -1,4 +1,4 @@
-from base_validators import check_existence, check_parameter_type, check_in_enum
+from base_validators import check_existence, check_parameter_type, check_in_enum, check_max, check_min, check_max_length, check_min_length
 from pagarme_lib_exception import PagarmeLibException
 
 def check_api_key(func):
@@ -13,20 +13,41 @@ def check_schema(schema):
         def parameters_wrapper(parameters_list):
             errors = []
             for key, constraints in schema.iteritems():
-                if 'type' in constraints:
-                    parameter_type = constraints['type']
-                    error = check_parameter_type(key, parameter_type, parameters_list)
-                    if error is not None: 
-                        errors.append(error)
-                if 'required' in constraints:
-                    error = check_existence(key, parameters_list)
-                    if error is not None: 
-                        errors.append(error)
-                if 'enum' in constraints:
-                    enum_list = constraints['enum']
-                    error = check_in_enum(key, enum_list, parameters_list)
-                    if error is not None: 
-                        errors.append(error)
+                if key in parameters_list:
+                    if 'type' in constraints:
+                        parameter_type = constraints['type']
+                        error = check_parameter_type(key, parameter_type, parameters_list)
+                        if error is not None: 
+                            errors.append(error)
+                    if 'required' in constraints:
+                        error = check_existence(key, parameters_list)
+                        if error is not None: 
+                            errors.append(error)
+                    if 'enum' in constraints:
+                        enum_list = constraints['enum']
+                        error = check_in_enum(key, enum_list, parameters_list)
+                        if error is not None:
+                            errors.append(error)
+                    if 'max' in constraints:
+                        max_value = constraints['max']
+                        error = check_max(key, max_value, parameters_list)
+                        if error is not None:
+                            errors.append(error)
+                    if 'min' in constraints:
+                        min_value = constraints['min']
+                        error = check_max(key, min_value, parameters_list)
+                        if error is not None:
+                            errors.append(error)
+                    if 'max_length' in constraints:
+                        max_length = constraints['max']
+                        error = check_max_length(key, max_length, parameters_list)
+                        if error is not None:
+                            errors.append(error)
+                    if 'min_length' in constraints:
+                        min_length = constraints['min']
+                        error = check_min_length(key, min_value, parameters_list)
+                        if error is not None:
+                            errors.append(error)
             if len(errors) > 0:
                 raise PagarmeLibException(errors)
             else:
