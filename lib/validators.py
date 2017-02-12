0dce2c94c5bc0452016e00ce50f528a7609608ff
Henrique Kano <henriquekano@hotmail.com>
2017-02-12 17:32:05 -0200

    Refactoring error adding on validations

--- lib/validators.py
+++ lib/validators.py
@@ -1,69 +1,95 @@
 from base_validators import (
-    check_existence, check_parameter_type, check_in_enum, check_max, check_min, check_max_length, check_min_length
-    )
+    check_existence,
+    check_parameter_type,
+    check_in_enum,
+    check_max,
+    check_min,
+    check_max_length,
+    check_min_length
+)
 from pagarme_lib_exception import PagarmeLibException
+
+
+def _add_errors_to_list(errors_list, *args):
+    errors = list(errors_list)
+    for error in args:
+        if error is not None:
+            errors.append(error)
+    return errors
+
 
 def check_api_key(func):
     def wrapper(parameters_dict):
         errors = []
-        error = check_existence('api_key', parameters_dict)
-        if error is not None: 
-            errors.append(error)
-        error = check_parameter_type('api_key', str, parameters_dict)
-        if error is not None: 
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_existence('api_key', parameters_dict),
+            check_parameter_type('api_key', str, parameters_dict)
+        )
         if (len(errors) > 0):
             raise PagarmeLibException(errors)
         return func(parameters_dict)
     return wrapper
 
+
 def _check_parameters_constraints(parameter_name, parameter_constraints, parameters_dict):
     errors = []
     if 'type' in parameter_constraints:
         parameter_type = parameter_constraints['type']
-        error = check_parameter_type(parameter_name, parameter_type, parameters_dict)
-        if error is not None: 
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_parameter_type(
+                parameter_name, parameter_type, parameters_dict)
+        )
     if 'required' in parameter_constraints:
-        error = check_existence(parameter_name, parameters_dict)
-        if error is not None: 
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_existence(parameter_name, parameters_dict)
+        )
     if 'enum' in parameter_constraints:
         enum_list = parameter_constraints['enum']
-        error = check_in_enum(parameter_name, enum_list, parameters_dict)
-        if error is not None:
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_in_enum(parameter_name, enum_list, parameters_dict)
+        )
     if 'max' in parameter_constraints:
         max_value = parameter_constraints['max']
-        error = check_max(parameter_name, max_value, parameters_dict)
-        if error is not None:
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_max(parameter_name, max_value, parameters_dict)
+        )
     if 'min' in parameter_constraints:
         min_value = parameter_constraints['min']
-        error = check_min(parameter_name, min_value, parameters_dict)
-        if error is not None:
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_min(parameter_name, min_value, parameters_dict)
+        )
     if 'max_length' in parameter_constraints:
         max_length = parameter_constraints['max_length']
-        error = check_max_length(parameter_name, max_length, parameters_dict)
-        if error is not None:
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_max_length(parameter_name, max_length, parameters_dict)
+        )
     if 'min_length' in parameter_constraints:
         min_length = parameter_constraints['min_length']
-        error = check_min_length(parameter_name, min_length, parameters_dict)
-        if error is not None:
-            errors.append(error)
+        errors = _add_errors_to_list(
+            errors,
+            check_min_length(parameter_name, min_length, parameters_dict)
+        )
     return errors
+
 
 def check_special_constraints():
     return []
+
 
 def _check_schema_recursive(schema, parameters_dict):
     errors = []
     for parameter_name, constraints in schema.iteritems():
         if parameter_name in parameters_dict:
-            errors = _check_parameters_constraints(parameter_name, constraints, parameters_dict)
-            #check sub schemas
+            errors = _check_parameters_constraints(
+                parameter_name, constraints, parameters_dict)
+            # check sub schemas
             if 'schema' in constraints:
                 sub_schema = constraints['schema']
                 sub_object = parameters_dict.get(parameter_name, {})
@@ -71,6 +97,7 @@ def _check_schema_recursive(schema, parameters_dict):
         else:
             errors += check_special_constraints()
     return errors
+
 
 def check_schema(schema):
     def function_wrapper(func):
