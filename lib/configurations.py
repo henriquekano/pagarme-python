9454af52b6b1e8e1ed7e903c6e8b5c68baf66748
Henrique Kano <henriquekano@hotmail.com>
2017-02-13 00:06:58 -0200

    Refactor configuration methods

--- lib/configurations.py
+++ lib/configurations.py
@@ -1,8 +1,13 @@
-endpoint = 'https://api.pagar.me'
-version = '1'
-api_key = ''
-encryption_key = ''
+_configuration = {
+	'endpoint': 'https://api.pagar.me',
+	'version': '1',
+	'api_key': '',
+	'encryption_key': ''
+}
 
 def PagarMeInit(new_api_key, new_encryption_key = ''):
-    api_key = new_api_key
-    encryption_key = new_encryption_key
+    _configuration['api_key'] = new_api_key
+    _configuration['encryption_key'] = new_encryption_key
+
+def get_full_url(path):
+	return _configuration['endpoint'] + '/' + _configuration['version'] + path
--- lib/transactions.py
+++ lib/transactions.py
@@ -1,12 +1,15 @@
 from schema.transaction import schema_transaction
 from validators import check_schema, check_api_key
 from response_formatter import format_datetimes
-from configurations import api_key, endpoint, version
+from request_formatter import ensure_api_key
+from configurations import get_full_url
 import requests
 
+@ensure_api_key
 @check_api_key
 @check_schema(schema_transaction)
 @format_datetimes
 def transaction_create(parameters):
-    r = requests.post(endpoint + '/' + version + '/' + 'transactions', data = parameters)
+    url = get_full_url('/transactions')
+    r = requests.post(url, data = parameters)
     return r.json()
