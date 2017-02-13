94e63d2a5ad8904a00fcc8cf52ff41a09ad6dfe8
Henrique Kano <henriquekano@hotmail.com>
2017-02-13 00:08:42 -0200

    Refactor response formatter dict get method

--- lib/response_formatter.py
+++ lib/response_formatter.py
@@ -1,9 +1,13 @@
 import dateutil.parser
 
 def format_datetimes(func):
-    def parameters_wrapper(parameters_list):
+    def _datetime_formatter(parameters_list):
         return_value = func(parameters_list)
-        return_value['date_created'] = dateutil.parser.parse(return_value['date_created'])
-        return_value['date_updated'] = dateutil.parser.parse(return_value['date_updated'])
+        date_created = return_value.get('date_created')
+        date_updated = return_value.get('date_updated')
+        if date_created is not None:
+        	return_value['date_created'] = dateutil.parser.parse(date_created)
+        if date_updated is not None:
+        	return_value['date_updated'] = dateutil.parser.parse(date_updated)
         return return_value
-    return parameters_wrapper
+    return _datetime_formatter
