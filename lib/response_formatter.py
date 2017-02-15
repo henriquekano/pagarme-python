f93754067126217fe298c45845683a20d0ce35b7
Henrique Kano <henriquekano@hotmail.com>
2017-02-15 01:26:50 -0200

    Improving response datetime formatter

--- lib/response_formatter.py
+++ lib/response_formatter.py
@@ -1,13 +1,29 @@
 import dateutil.parser
+from datetime import datetime
+
+def _traverse_json_recursive(parameters_list, format_value):
+    for key, value in parameters_list.items():
+        if type(value) is dict:
+            _traverse_json_recursive(value, format_value)
+        else:
+            parameters_list[key] = format_value(key, value)
+
+def _format_isodatetime(value):
+    return dateutil.parser.parse(value)
+
+def _format_month_year_date(value):
+    return datetime.strptime(value, '%m%y')
+
+def _format_datetime_based_on_key(key, value):
+    if key in ['date_created', 'date_updated', 'boleto_expiration_date']:
+        return _format_isodatetime(value)
+    if key in ['expiration_date']:
+        return _format_month_year_date(value)
+    return value
 
 def format_datetimes(func):
     def _datetime_formatter(parameters_list):
-        return_value = func(parameters_list)
-        date_created = return_value.get('date_created')
-        date_updated = return_value.get('date_updated')
-        if date_created is not None:
-        	return_value['date_created'] = dateutil.parser.parse(date_created)
-        if date_updated is not None:
-        	return_value['date_updated'] = dateutil.parser.parse(date_updated)
-        return return_value
+        response = func(parameters_list)
+        _traverse_json_recursive(response, _format_datetime_based_on_key)
+        return response
     return _datetime_formatter
---
+++ tests/response_formatter.py
@@ -0,0 +1,32 @@
+import unittest
+from lib.response_formatter import format_datetimes
+from datetime import datetime
+
+class ResponseFormatter(unittest.TestCase):
+
+	def test_datetime_formatter(self):
+		datetime_dict = {
+			'date_created': '2015-08-27T17:53:56.000Z',
+			'date_updated': '2015-08-27T17:53:56.000Z',
+			'boleto_expiration_date': '2015-08-27T17:53:56.000Z',
+			'expiration_date': '2015-08-27T17:53:56.000Z',
+			'expiration_date': '1220',
+			'sub_object': {
+				'date_created': '2015-08-27T17:53:56.000Z',
+				'date_updated': '2015-08-27T17:53:56.000Z',
+				'boleto_expiration_date': '2015-08-27T17:53:56.000Z',
+				'expiration_date': '2015-08-27T17:53:56.000Z',
+				'expiration_date': '1220'
+			}
+		}
+		formatted = format_datetimes(lambda x: x)(datetime_dict)
+		self.assertTrue(type(datetime_dict.get('date_created')) is datetime)
+		self.assertTrue(type(datetime_dict.get('date_updated')) is datetime)
+		self.assertTrue(type(datetime_dict.get('boleto_expiration_date')) is datetime)
+		self.assertTrue(type(datetime_dict.get('expiration_date')) is datetime)
+		self.assertTrue(type(datetime_dict.get('expiration_date')) is datetime)
+		self.assertTrue(type(datetime_dict.get('sub_object').get('date_created')) is datetime)
+		self.assertTrue(type(datetime_dict.get('sub_object').get('date_updated')) is datetime)
+		self.assertTrue(type(datetime_dict.get('sub_object').get('boleto_expiration_date')) is datetime)
+		self.assertTrue(type(datetime_dict.get('sub_object').get('expiration_date')) is datetime)
+		self.assertTrue(type(datetime_dict.get('sub_object').get('expiration_date')) is datetime)
