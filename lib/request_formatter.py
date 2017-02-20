f71080e48035b8b6647a24373994d84090532699
Henrique Kano <henriquekano@hotmail.com>
2017-02-19 22:09:27 -0300

    refactor request_formatter: name and bool - not none

--- lib/request_formatter.py
+++ lib/request_formatter.py
@@ -7,21 +7,15 @@ def ensure_api_key(func):
         return func(parameters_dict)
     return _add_api_key
 
-def format_to_dict(func):
-    def _group_parameters(**kwargs):
-        parameters = {}
-        for key, value in kwargs:
-            if type(value) is dict:
-                parameters.update(value)
-            elif value is not None:
-                parameters[key] = value
-        return parameters
-    return _group_parameters
+def _filter_none_values(**kwargs):
+    return {key: value for key, value in kwargs.iteritems() if value is not None}
 
 def _format_boolean_to_string(key, value):
-    return str(value)
+    if type(value) is bool:
+        return str(value).lower()
+    return value
 
-def format_booleans(func):
+def _format_booleans(func):
     def _formatter(parameters_dict):
         _traverse_json_recursive(parameters_dict, _format_boolean_to_string)
         return func(parameters_dict)
